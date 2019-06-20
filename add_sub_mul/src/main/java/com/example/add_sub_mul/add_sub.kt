472d42665ab09032cd5d33607c47bdcc40559f9e
Sujit More <sujitmore21@gmail.com>
2019-06-20 17:18:42 +0530

    add code

--- add_sub_mul/src/main/java/com/example/add_sub_mul/add_sub.kt
+++ add_sub_mul/src/main/java/com/example/add_sub_mul/add_sub.kt
@@ -13,29 +13,43 @@ class add_sub : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_sub)
 
-        fun main(args: Array<String>) {
 
-            val et1 = findViewById<EditText>(R.id.et1)
+        val et1 = findViewById<EditText>(R.id.et1)
 
-            val et2 = findViewById<EditText>(R.id.et2)
+        val et2 = findViewById<EditText>(R.id.et2)
 
-            val btadd = findViewById<Button>(R.id.add)
+        val btadd = findViewById<Button>(R.id.add)
 
-            val btsub = findViewById<Button>(R.id.sub)
+        val btsub = findViewById<Button>(R.id.sub)
 
-            val btmul = findViewById<Button>(R.id.mul)
+        val btmul = findViewById<Button>(R.id.mul)
 
-            val btdiv = findViewById<Button>(R.id.div)
+        val btdiv = findViewById<Button>(R.id.div)
 
-            val txSt = findViewById<TextView>(R.id.txSt)
+        val txSt = findViewById<TextView>(R.id.txSt)
 
-            btadd.setOnClickListener {
-                if (et1.text.toString() == "" && et2.text.toString()==""){
-                    txSt.text = ""
-                }else{
-                    txSt.text = "Error"
-                }
-            }
+        btadd.setOnClickListener {
+
+            val num1 = et1.text.toString()
+            val num2 = et2.text.toString()
+
+             val add = Integer.parseInt(num1)+Integer.parseInt(num2)
+            txSt.text = ""+add
+
+            val sub = Integer.parseInt(num1)+Integer.parseInt(num2)
+            txSt.text = ""-sub
+
+            val mul = Integer.parseInt(num1)+Integer.parseInt(num2)
+            txSt.text = ""*mul
+
+            val num = Integer.parseInt(num1)+Integer.parseInt(num2)
+            txSt.text = ""/div
+
+
+
+
         }
     }
 }
+
+
