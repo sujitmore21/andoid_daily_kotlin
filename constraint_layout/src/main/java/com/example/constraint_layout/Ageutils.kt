b88171d2cc71ab9b50a3658a8453e99906267d84
Sujit More <sujitmore21@gmail.com>
2019-06-25 12:59:31 +0530

    Constraint layout: age calender min & max

--- constraint_layout/src/main/java/com/example/constraint_layout/Ageutils.kt
+++ constraint_layout/src/main/java/com/example/constraint_layout/Ageutils.kt
@@ -15,7 +15,7 @@ object ageobj {
 
         val age = today.get(Calendar.YEAR) - dob.get(Calendar.YEAR)
         if (today.get(Calendar.DAY_OF_YEAR)< dob.get(Calendar.DAY_OF_YEAR)) {
-            age
+
 
         }
 
